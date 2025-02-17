76244ad14b8812d61a7e23302c4d76f3e060523a
manh <manhpd.hut@gmail.com>
2025-02-17 17:01:35 +0700

    build: add form validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor() {
+  }
+
+  login(formData: NgForm) {
+    const form = formData.form;
+    if (form.invalid) {
+      return;
+    }
+
+    const email =  form.value.email;
+    const password = form.value.password;
+    
+    console.log(email, password);
+  }
+}
