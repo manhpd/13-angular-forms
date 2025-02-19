2eff4760faa524004866be719d3c94d8b9a0cff6
manh <manhpd.hut@gmail.com>
2025-02-19 15:35:59 +0700

    build: return to reactive form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,26 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  constructor() {
-  }
-
-  login(formData: NgForm) {
-    const form = formData.form;
-    if (form.invalid) {
-      return;
-    }
-
-    const email =  form.value.email;
-    const password = form.value.password;
-    
-    console.log(email, password);
-  }
-}
+export class LoginComponent {}
