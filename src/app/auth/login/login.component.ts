b538ffe8aec2c7eec557cf83800c3415829377da
manh <manhpd.hut@gmail.com>
2025-02-20 16:50:30 +0700

    build: add more field and validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, delay, of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.indexOf('?') === -1) {
+    return { mustContainQuestionMark: true };
+  }
+  return null;
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value === 'test@example.com') {
+      return of({ emailIsUnique: true });
+  }
+  return of(null);
+}
+
+let initialEmail = '';
+
+const savedLoginForm = window.localStorage.getItem('saved-login-form');
+if (savedLoginForm) {
+  initialEmail = JSON.parse(savedLoginForm).email;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+
+
+export class LoginComponent implements OnInit {
+
+  private destroyRef = inject(DestroyRef);
+
+  loginForm = new FormGroup(
+    {
+      email: new FormControl(initialEmail,{
+        validators: [Validators.required, Validators.email],
+        updateOn: 'blur',
+        asyncValidators: [
+          emailIsUnique
+        ],
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required, 
+          Validators.minLength(6),
+          mustContainQuestionMark
+        ],
+        
+        updateOn: 'blur'
+      }),
+    }
+  );
+
+  get emailIsInvalid() {
+    return this.loginForm.controls.email.touched && this.loginForm.controls.email.dirty && this.loginForm.controls.email.invalid;
+  }
+
+  get passwordIsInvalid() {
+    return this.loginForm.controls.password.touched && this.loginForm.controls.password.dirty && this.loginForm.controls.password.invalid;
+  }
+
+  ngOnInit() {
+    const subscrition = this.loginForm.valueChanges.pipe(debounceTime(500)).subscribe({
+      next: (value) => {
+        window.localStorage.setItem('saved-login-form', JSON.stringify({
+          email: value.email
+        }));
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+        subscrition.unsubscribe();
+      }
+    );
+
+    // const savedLoginForm = window.localStorage.getItem('saved-login-form');
+    // if (savedLoginForm) {
+    //   this.loginForm.patchValue(JSON.parse(savedLoginForm));
+    // }
+  }
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
+}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,124 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    }),
+
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      }),
+    }, {
+      validators: [
+        (formGroup) => {
+          const password = formGroup.get('password');
+          const confirmPassword = formGroup.get('confirmPassword');
+          if (password?.value !== confirmPassword?.value) {
+            confirmPassword?.setErrors({ passwordsDoNotMatch: true });
+          } else {
+            confirmPassword?.setErrors(null);
+          }
+          return null;
+        }
+      ]
+    }),
+   
+    firstName: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    lastName: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+
+    address: new FormGroup({
+      number: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.pattern(/^[0-9]*$/)
+        ]
+      }),
+      street: new FormControl('', {
+        validators: [
+          Validators.required
+        ]
+      }),
+      city: new FormControl('', {
+        validators: [
+          Validators.required
+        ]
+      }),
+      postalCode: new FormControl('', {
+        validators: [
+          Validators.required
+        ]
+      }),
+    }),
+    
+    role: new FormControl <'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
+      validators: [
+        Validators.required
+      ] 
+    }),
+    agree : new FormControl(false, {
+      validators: [
+        Validators.requiredTrue
+      ]
+    })
+  });
+
+  get emailIsInvalid() {
+    const email = this.signupForm.get('email');
+    return email?.invalid && email.touched;
+  }
+
+  get passwordIsInvalid() {
+    const password = this.signupForm.get('password');
+    return password?.invalid && password.touched;
+  }
+
+  get confirmPasswordIsInvalid() {
+    const confirmPassword = this.signupForm.get('confirmPassword');
+    return confirmPassword?.invalid && confirmPassword.touched;
+  }
+
+  get passwordsDoNotMatch() {
+    const password = this.signupForm.get('password');
+    const confirmPassword = this.signupForm.get('confirmPassword');
+    return password?.value !== confirmPassword?.value;
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
+
+  onReset() {
+    this.signupForm.reset();
+  }
+}
